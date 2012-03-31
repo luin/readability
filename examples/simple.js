78d30df8b3b784abe63c8487bc659301d3ba4bb6
Zihua Li <i@zihua.li>
2012-03-31 20:19:07 +0800

    update the example

--- examples/simple.js
+++ examples/simple.js
@@ -1,9 +1,13 @@
-var readability = require('../src/readability');
+var readability = require('../src/readability')
+  , fs = require('fs')
+
+// uncoment the following line to print the debug info to console.
+// readability.debug(true);
 
 
-readability.debug(true);
-
-readability.read('http://howtonode.org/really-simple-file-uploads', function(err, read) {
-  console.log(read.getContent());
-  console.log(read.getTitle());
+readability.read('http://colorlines.com/archives/2011/08/dispatch_from_angola_faith-based_slavery_in_a_louisiana_prison.html',
+function(err, read) {
+  var dom = read.getDocument();
+  var html = '<html><head><meta charset="utf-8"><title>'+dom.title+'</title></head><body><h1>'+read.getTitle()+'</h1>'+read.getContent()+'</body></html>';
+  console.log(html);
 });
