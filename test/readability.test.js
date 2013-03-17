9e13c159101c39dcdd8812ce368a273183c5dd58
Oliver Joseph Ash <oliverash@me.com>
2013-03-17 18:19:07 +0000

    Check for embed src before performing search method, workaround for #5

--- test/readability.test.js
+++ test/readability.test.js
@@ -15,5 +15,16 @@ describe('node-readability', function () {
       html.should.include('<title>Dispatch From Angola: Faith-Based Slavery in a Louisiana Prison - COLORLINES</title>')
       done()
     });
+  });
+
+  // @todo: Find the real edge case for this issue
+  it('should get document at http://www.theverge.com/2013/3/17/4113714/60-minutes-jack-dorsey-profile-on-cbs-tonight', function (done) {
+    readability.read('http://www.theverge.com/2013/3/17/4113714/60-minutes-jack-dorsey-profile-on-cbs-tonight', function(err, read) {
+      if (err) return done(err)
+      var dom = read.getDocument();
+      var html = '<html><head><meta charset="utf-8"><title>'+dom.title+'</title></head><body><h1>'+read.getTitle()+'</h1>'+read.getContent()+'</body></html>';
+      html.should.include('<title>\'60 Minutes\' profile of Twitter creator Jack Dorsey airing tonight at 7PM ET/PT | The Verge</title>')
+      done()
+    });
   })
-})
+});
