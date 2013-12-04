a47e1a9d891050ce579ec70e5abcefb0f8919dfa
Luin <i@zihua.li>
2013-12-04 19:54:27 +0900

    Update examples

--- examples/simple.js
+++ examples/simple.js
@@ -1,12 +1,8 @@
-var readability = require('../src/readability')
+var read = require('../src/readability');
 
-// uncoment the following line to print the debug info to console.
-// readability.debug(true);
-
-
-readability.read('http://colorlines.com/archives/2011/08/dispatch_from_angola_faith-based_slavery_in_a_louisiana_prison.html',
+read('http://colorlines.com/archives/2011/08/dispatch_from_angola_faith-based_slavery_in_a_louisiana_prison.html',
 function(err, read) {
-  var dom = read.getDocument();
-  var html = '<html><head><meta charset="utf-8"><title>'+dom.title+'</title></head><body><h1>'+read.getTitle()+'</h1>'+read.getContent()+'</body></html>';
+  var dom = read.document;
+  var html = '<html><head><meta charset="utf-8"><title>'+dom.title+'</title></head><body><h1>'+read.title+'</h1>'+read.content+'</body></html>';
   console.log(html);
 });
