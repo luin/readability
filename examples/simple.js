ba51e1d160d792efde99d84965b19577b7d8767e
Zihua Li <i@zihua.li>
2014-01-16 21:33:32 +0900

    Update docs and examples for the `meta` object.

--- examples/simple.js
+++ examples/simple.js
@@ -1,8 +1,9 @@
 var read = require('../src/readability');
 
 read('http://colorlines.com/archives/2011/08/dispatch_from_angola_faith-based_slavery_in_a_louisiana_prison.html',
-function(err, read) {
-  var dom = read.document;
-  var html = '<html><head><meta charset="utf-8"><title>'+dom.title+'</title></head><body><h1>'+read.title+'</h1>'+read.content+'</body></html>';
+function(err, article, meta) {
+  var dom = article.document;
+  var html = '<html><head><meta charset="utf-8"><title>'+dom.title+'</title></head><body><h1>'+article.title+'</h1>'+article.content+'</body></html>';
   console.log(html);
+  console.log(meta);
 });
