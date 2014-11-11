dafe50b59b18497475c4946d958c15c15cad53f8
ybak.yf <ybakswu@gmail.com>
2014-11-11 16:25:41 +0800

    fix CI build mocha test failed case.

--- test/charset.js
+++ test/charset.js
@@ -1,14 +1,22 @@
 require('./mock-helpers.js');
+
+var fs = require('fs');
+var path = require('path');
 var read = require('../src/readability');
 
+var filePath = path.join(__dirname, 'test.html');
+
+
 describe('charset', function () {
-  describe('setted in the html', function() {
-    it('should convert the page to utf-8', function(done) {
-      read('http://tech.sina.com.cn/it/2014-01-16/04049100515.shtml', function(err, read) {
-        read.content.should.include('谷歌');
-        read.title.should.include('谷歌');
-        done();
-      });
+    describe('setted in the html', function () {
+        it('should convert the page to utf-8', function (done) {
+            fs.readFile(filePath, {encoding: 'utf-8'}, function (err, html) {
+                read(html, function (err, read) {
+                    read.content.should.include('谷歌');
+                    read.title.should.include('谷歌');
+                    done();
+                });
+            });
+        });
     });
-  });
 });
