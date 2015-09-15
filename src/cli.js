4a232b98354f6000295522e92c5e3b9281850446
git@aroneiermann.de <git@aroneiermann.de>
2015-09-15 20:44:05 +0200

    Added CLI ability.

---
+++ src/cli.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env iojs
+var read = require("./readability.js");
+var argv = require("minimist")(process.argv.slice(2));
+
+if(argv.h){
+  process.stdout.write(
+    "luin readability\n" +
+    "Usage: readability --url [URL] " +
+    "prints readability version of [URL].\n" +
+    "Usage: readability             " +
+    "reads HTML from stdin and prints readable version to stdout.\n" +
+    "Usage: readability -h          " +
+    "prints this help.\n"
+  );
+  return;
+}
+
+var callback = function(err, article){
+  if(err)
+    console.error(err);
+  process.stdout.write(article.html);
+}
+
+if(typeof argv.url === 'string'){
+  read(argv.url, callback);
+} else {
+  var html;
+  process.stdin.on("data", function(chunk){
+    html += chunk;
+  });
+  process.stdin.on("end", function(){
+    read(html, callback);
+  });
+}
