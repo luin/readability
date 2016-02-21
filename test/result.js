3248c7dbd7fafd5f899632179cd7676dd2dcbdcf
Kashif Eqbal <kashifeqbal@hotmail.com>
2016-02-21 23:03:24 +0530

    Readability Logic changed completely
    Added Mozilla Readability code and redirection and Page not found error
    status.

--- test/result.js
+++ test/result.js
@@ -1,42 +1,28 @@
 require('./mock-helpers.js');
-var read = require('../src/readability');
+var read = require('../src/readability-output');
+var should = require('should');
 
 describe('result', function () {
   it('should return the result', function (done) {
     read('http://colorlines.com/archives/2011/08/dispatch_from_angola_faith-based_slavery_in_a_louisiana_prison.html', function (err, read) {
-      var dom = read.document;
-      var html = '<html><head><meta charset="utf-8"><title>' + dom.title +
-        '</title></head><body><h1>' + read.title + '</h1>' + read.content + '</body></html>';
+      var html = '<html><head><meta charset="utf-8"><title>' + read.title + '</title></head><body><h1>' + read.title + '</h1>' + read.content + '</body></html>';
       html.should.include('<title>Dispatch From Angola: Faith-Based Slavery in a Louisiana Prison');
-      read.close.should.be.a.Function
-      read.close();
       done();
     });
   });
 
   it('should get document with frames', function (done) {
     read('http://www.whitehouse.gov/', function (err, read) {
-      var dom = read.document;
+      should.not.exist(err);
+      should.exist(read);
       read.title.should.equal('The White House');
-      read.close.should.be.a.Function
-      read.close();
       done();
     });
   });
-
-  it('should handle frames', function (done) {
-    read('<html><body><frame /><frame />Hello world!</body></html>', function (err, read) {
-      read.document.body.innerHTML.should.include('Hello world!');
-      read.close.should.be.a.Function
-      read.close();
-      done();
-    });
-  });
-  it('should get author', function (done) {
-    read('http://www.theengineer.co.uk/skills-shortage-leaves-up-to-5000-automotive-industry-jobs-vacant-claims-new-report/', function (err, read) {
-      console.log(read.author);
-      read.author.should.be.a.String;
-      read.close();
+  it('should handle URL', function (done) {
+    read('https://campustechnology.com/articles/2016/02/10/new-textbook-liberation-fund-will-help-faculty-ditch-high-priced-textbooks.aspx', function (err, read) {
+      should.not.exist(err);
+      should.exist(read);
       done();
     });
   });
---
+++ test/url_test.js
@@ -0,0 +1,36 @@
+require('./mock-helpers.js');
+var read = require('../src/readability-output');
+var should = require('should');
+
+describe('Error, Redirection and image test', function () {
+  it('should give error - page not found', function (done) {
+    read('http://www.ihsmaritime360.com/article/17325/jes-international-still-in-red-for-2014', function (err, read) {
+      err.status.should.be.within(400, 511);
+      done();
+    });
+  });
+  it('should give error - page not found', function (done) {
+    read('http://www.theepochtimes.com/n3/1305716-live-in-a-porsche-designer-labels-draw-miami-home-buyers/', function (err, read) {
+      err.status.should.be.within(400, 511);
+      done();
+    });
+  });
+  it('should give error - page not found', function (done) {
+    read('http://www.rewmag.com/Article.aspx?article_id=183411', function (err, read) {
+      err.status.should.be.within(400, 511);
+      done();
+    });
+  });
+  it('should redirect', function (done) {
+    read('http://rss.justia.com/~r/AtlantaEmploymentLawyerBlogCom/~3/GQPPBTA9pSw/employment-discrimination-may-involve-many-different-types-of-conduct.html', function (err, read) {
+      read.redirection.should.be.equal(true);
+      done();
+    });
+  });
+  it('should have image', function (done) {
+    read('http://www.bakingbusiness.com/articles/news_home/Financial-Performance/2015/04/Gourmet_brands_boost_Barry_Cal.aspx?ID=%7BAEDD6EC2-3D41-49C0-9CB7-27E9BF7EC9F6%7D', function (err, read) {
+      should.exist(read.lead_image_url);
+      done();
+    });
+  });
+});
